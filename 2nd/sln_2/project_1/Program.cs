05e40d84f039717eb6c4896b649a7f5140c951c5
MianShang <napori03@naver.com>
2025-03-11 11:50:26 +0900

    2주차 수업

---
+++ 2nd/sln_2/project_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Console; /// Console.WriteLine -> WriteLine으로 줄일 수 있음
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// † namespace 영역
+
+namespace project_1 ///프로젝트 이름 어떤 프로젝트를 시작할때 namespace가 있어야한다.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine(275); ///Console.WriteLine 줄바꿈 출력문
+                            ///Console.Write 줄안바꾸고 그대로 출력
+            Console.WriteLine('A');
+            Console.WriteLine("Program");
+        }
+    }
+}
